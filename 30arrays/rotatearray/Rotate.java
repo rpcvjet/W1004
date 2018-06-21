a4f1232af752747091d5387dd0971963c429e34e
Kenneth Edwards <kennethedwards@Kenneths-MacBook-Pro.local>
2018-06-20 23:49:22 -0400

    edits and added folder

--- 30arrays/rotatearray/Rotate.java
+++ 30arrays/rotatearray/Rotate.java
@@ -4,9 +4,6 @@ class Rotate {
   public static void main(String[] args) {
 
     int[] a = {2, 3, 5, 7, 11, 13};
-    int last = a.length - 1;
-    // 3,5,7,11,13,2
-
     int temp = a[0];
 
     for(int i = 1; i <= a.length -1; i++) {
